85bd415aedf8a33af2d3821f267588b8582356b6
yepeng <yepeng@utry.cn>
2016-03-30 09:18:49 +0800

    5.6.3

---
+++ javascript高级程序设计/5.6.3/htmlEscape.js
@@ -0,0 +1,12 @@
+/**
+ * html替换
+ * @param text
+ */
+function htmlEscape(text){
+    return text.replace(/[<>&/g,function(match,pos,originalText){
+        switch (match){
+            case "<":
+                return "&lt;";
+        }
+    })
+}
